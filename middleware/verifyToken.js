b4b7bad24a7091ec240a8090b65a9bdcd176a11d
Oleg Stepanov <olezhka106s@gmail.com>
2022-03-28 19:31:24 +0300

    Middleware fix

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function auth(req, res, next) {
     const token = req.header('auth-token');
-    if(!token) return res.status(403).send({ sucess: false, message: 'Auth user access Denied'});
+    if(!token) return res.status(403).send({ success: false, message: 'Auth user access Denied'});
 
     try {
         const verified = jwt.verify(token, process.env.AUTH_TOKEN);
         req.user = verified;
         next();
     }catch(err){
-        res.status(403).send({ sucess: false, message: 'Invalid auth token'});
+        res.status(403).send({ success: false, message: 'Invalid auth token'});
     }
 }
