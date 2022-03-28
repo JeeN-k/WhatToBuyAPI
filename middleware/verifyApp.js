3cb4acb982e319b2675b3b3d3dad43842dd9739b
Oleg Stepanov <olezhka106s@gmail.com>
2022-03-28 19:01:18 +0300

    Fix middleware

--- middleware/verifyApp.js
+++ middleware/verifyApp.js
@@ -1,10 +1,10 @@
 module.exports = function appAuth(req, res, next) {
-    // const token = req.header('app-token');
-    // if(!token) return res.status(403).send( {sucess: false, message: 'Auth app access Denied' })
+    const token = req.header('app-token');
+    if(!token) return res.status(403).send( {sucess: false, message: 'Auth app access Denied' })
 
     try {
-        // const verified = token == process.env.IOS_TOKEN
-        // req.app = verified
+        const verified = token == process.env.IOS_TOKEN
+        req.app = verified
         next();
     } catch(err) {
         res.status(403).send({ sucess: false, message: 'Invalid app token'});
